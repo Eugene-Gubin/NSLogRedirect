1ab049da28cd92ef08b91445ad6eb888345abf3b
Евгений Губин <hemet.mail@gmail.com>
2014-05-22 12:18:39 +0400

    Initial Commit

---
+++ NSLogRedirect/NSLogRedirect.h
@@ -0,0 +1,13 @@
+//
+//  NSLogRedirect.h
+//  NSLogRedirect
+//
+//  Created by Евгений Губин on 22.05.14.
+//  Copyright (c) 2014 yillio.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSLogRedirect : NSObject
+
+@end
---
+++ NSLogRedirectTests/NSLogRedirectTests.m
@@ -0,0 +1,34 @@
+//
+//  NSLogRedirectTests.m
+//  NSLogRedirectTests
+//
+//  Created by Евгений Губин on 22.05.14.
+//  Copyright (c) 2014 yillio.com. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSLogRedirectTests : XCTestCase
+
+@end
+
+@implementation NSLogRedirectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
